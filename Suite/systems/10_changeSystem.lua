424a44b39bd755e46c03481c0dd036b82f109b8d
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2020-02-17 21:02:25 -0500

    remove buggy team create support

--- Suite/systems/10_changeSystem.lua
+++ Suite/systems/10_changeSystem.lua
@@ -17,29 +17,12 @@ return function(Sunshine, entity)
                 change.teamCreateUpdate = false
                 handleChange(change.entity, change.componentName, change.propertyName, change.propertyValue,
                 change.alreadyChangedOnEntity)
-                if Sunshine.PluginNetworkClient.isEnabled() then
-                    Sunshine.PluginNetworkClient:fireAllClients(change.entity.core.scene.instance,
-                    change.entity.core.id, change.componentName, change.propertyName, change.propertyValue)
-                end
                 change.entity = nil
                 change.componentName = nil
                 change.propertyName = nil
                 change.propertyValue = nil
                 change.alreadyChangedOnEntity = nil
             end
-        end, entity)
-        local index = Sunshine.PluginNetworkClient:onClientEvent(function(_, sceneInstance, entityId,
-            componentName, propertyName, propertyValue)
-            for _, scene in pairs(Sunshine.scenes) do
-                if scene.instance == sceneInstance then
-                    local changedEntity = Sunshine:getEntityById(entityId, scene)
-                    change.teamCreateUpdate = true
-                    handleChange(changedEntity, componentName, propertyName, propertyValue)
-                end
-            end
-        end)
-        Sunshine:entityDestroy(function()
-            Sunshine.PluginNetworkClient:removeClientEvent(index)
         end, entity)
     end
 end
